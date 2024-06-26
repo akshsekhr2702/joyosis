9d3dded53fea0e1815a8ad1d2539421bac33290a
Varun Kumar Shekar <varun.k.shekar@gmail.com>
2024-06-26 21:36:38 +0530

    All issues solved,, good to go�

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-      <body>{children}</body>
-      </ThemeProvider>
+      {/* <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      > */}
+      <body>
+        {children}
+      </body>
+      {/* </ThemeProvider> */}
     </html>
   );
 }
